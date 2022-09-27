ad33da4205f321ed57df22a3dc2e54a1dc7361de
root <root@DESKTOP-MJT5QM4.localdomain>
2022-09-27 12:53:12 +0200

    1er commit

---
+++ exercice comparer.py
@@ -0,0 +1,6 @@
+# C'est quoi le résultat de print Resbool
+ResBool = (-22>=-18)
+#print (ResBool)
+#Régler le paramètre sur Vrai si 17<328 ou False si elle est pas
+ResBool = (17<328)
+print (ResBool)
